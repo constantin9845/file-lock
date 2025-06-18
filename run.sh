126c3d6ce5010fffb075c4884ec44582265a467f
marron <constantin.y@outlook.com>
2025-06-18 23:42:57 +0900

    update compile command

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-g++ -g -std=c++17 src/main.cpp src/fileHandler.cpp src/AES.cpp -o enc
-lldb ./enc
+g++ src/main.cpp src/fileHandler.cpp src/AES.cpp -std=c++17 -maes -mpclmul "-msse4.1" -O2  -o enc
+#g++ -g -std=c++17 -maes -mpclmul -msse4.1 -02 src/main.cpp src/fileHandler.cpp src/AES.cpp -o enc
+#lldb ./enc
+#gdb ./enc
